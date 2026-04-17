bafdfe28a6e175bde7a0b36df1ba2cff195cc6e0
melissatedesco <tedesco.melissa.its2@gmail.com>
2026-04-17 12:07:27 +0200

    prova

--- src/app/components/chat/chat.ts
+++ src/app/components/chat/chat.ts
@@ -3,7 +3,7 @@ import { isPlatformBrowser } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ChatService, ChatMessage } from '../../services/chat-service';
-
+// ciao
 @Component({
   selector: 'app-chat',
   standalone: true,
